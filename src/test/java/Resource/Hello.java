c4104f8896136f0c6afa01b5d31dc4738d3c4aaf
Deepak <d.saini012@gmail.com>
2024-09-09 10:52:17 +0530

    X User Commit

--- src/test/java/Resource/Hello.java
+++ src/test/java/Resource/Hello.java
@@ -10,36 +10,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		WebDriver driver = new ChromeDriver();
-		driver.get("");
-		String text= driver.swtichTo().alert().getText();
-		driver.swtichTo().alert().dismiss();
-		
-     List<WebElement> elements =  driver.findElements(By.xpath(""));
-     
-     for(WebElement e: elements)
-     {
-    	 if(e.getText().equalsIngnoreCase("India"))
-    	 {
-    		 e.click();
-    		 break;
-    	 }
-     }
-     
-    Set<String> windows = driver.getWindowHandles();
-    Iterator i = windows.iterator();
-    String parent = i.next();
-    String child = i.next();
-    driver.swtichTo.window(child);
-    String text= driver.findElement(By.xpath("")).getText();
-    
-    Response res=
-    		
-    	when().then().assertThat().
-    	String name= res.jsonPath().getString("name");
-    
-    
-    
+	System.out.println("Hello");
+	System.out.println("Hello1");
     
     
 		// TODO Auto"-generated method stub
